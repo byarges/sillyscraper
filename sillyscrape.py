e8ab60404caeca1d04b65b0709a3270b8795b037
My Guy <byarges@hotmail.com>
2021-12-11 21:46:01 -0800

    more filter words and some html changes

--- sillyscrape.py
+++ sillyscrape.py
@@ -10,10 +10,11 @@ import webbrowser
 from bs4 import BeautifulSoup
 
 thislist = []
-filterwords =["in","to", "the","News","for","of","on"]
+filterwords =["in","to", "the","News","for","of","on", "+", "and","a","by","with","is","as","be","after","are","at","or","&","|"]
 the_page = requests.get("https://www.foxnews.com/")
 soup = BeautifulSoup(the_page.content, 'html.parser')
-f = open("/var/www/html/index.html", "w")
+#f = open("/var/www/html/index.html", "w")
+f = open("index.html", "w")
 counter=0
 lengthofcounter=0
 
@@ -37,7 +38,10 @@ clean = Counter(y).most_common(100)
 
 print(Counter(y).most_common(100))
 
-f.write(str(clean))
+
+htmlfileFoxStart="<h1>Fox News Top 100 Words of the Hour: </h1>"
+f.write(htmlfileFoxStart+"<p>"+str(clean)+"</p>")
+
 
 
 f.close()
